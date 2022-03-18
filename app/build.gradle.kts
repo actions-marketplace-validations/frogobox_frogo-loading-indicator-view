6ee729da17bacc9ab8d5ee62c00bef6110652650
Faisal Amir <faisalamircs@gmail.com>
2022-03-18 14:05:45 +0700

    Update and rename build.gradle to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.wang.avi.sample"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 33
+        versionName = "2.1.2"
+
+        multiDexEnabled = true
+        vectorDrawables.useSupportLibrary = true
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":library"))
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+}
