0e8d0cf259d59d7681bee513662b9e4a975d9b97
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-03-18 18:09:51 +0700

    update: build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+
+    compileSdk = ProjectSetting.PROJECT_COMPILE_SDK
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 31
+        minSdk = ProjectSetting.PROJECT_MIN_SDK
+        targetSdk = ProjectSetting.PROJECT_TARGET_SDK
 
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
@@ -59,4 +60,31 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
    
     implementation("com.google.android.material:material:1.5.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = ProjectSetting.PROJECT_LIB_ID_SDK
+
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = ProjectSetting.MODULE_NAME_SDK
+
+                // Version Library Name (Example : "1.0.0")
+                version = ProjectSetting.PROJECT_VERSION_NAME
+
+            }
+        }
+    }
 }
